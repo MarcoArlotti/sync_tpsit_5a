46a76f951f161d5affad79fa963ae77597f3403c
SignorGatt0 <arlottimarco17@gmail.com>
2025-11-22 12:31:03 +0100

    finito l'esercizio 1

--- arlotti_01/espressione_regolare.py
+++ arlotti_01/espressione_regolare.py
@@ -1,17 +1,22 @@
-import re
+from re import Pattern,compile
 from stringa_input import creazione_stringa
+
+
 class espressione_regolare:
-    def __init__(self,espressione):
-        self.espressione = re.compile(espressione)
+    def __init__(self, espressione):
+        self.espressione = compile(espressione)
         self.stringa = creazione_stringa.stringa()
 
-    def match(self):
-        corrisponde = bool(re.pattern.match(self.espressione,self.stringa))
+    def match_stringa(self):
+        corrisponde = Pattern.match(self.espressione, self.stringa)
         if corrisponde:
             return "match"
         else:
             return "mismatch"
 
-espressione_regolare1 = espressione_regolare(r"(0[1-9]|[1-2][0-9]|3[0-1])\/(0[0-9]|1[0-2])\/20(\d{2})")
-risultato = espressione_regolare.match()
+# controllo di una data gg/mm/20aa
+espressione_regolare1 = espressione_regolare(
+    r"(0[1-9]|[1-2][0-9]|3[0-1])\/(0[0-9]|1[0-2])\/20(\d{2})"
+)
+risultato = espressione_regolare1.match_stringa()
 print(risultato)
