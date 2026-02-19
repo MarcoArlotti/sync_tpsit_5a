4f7211e3da8ce3b2e3e5a20042ba881f93bbafbe
MarcoArlotti <marco.arlotti@studenti.isissgobetti.it>
2026-02-19 16:41:03 +0100

    fix

--- arlotti_10/client.java
+++ arlotti_10/client.java
@@ -1,28 +1,23 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Client { // stesso nome del file
     public static void main(String[] args) {
-
-        Socket socket = null;
-        
-        InputStreamReader inputStreamReader = null;
-        OutputStreamWriter outputStreamWriter = null;
-
-        BufferedReader bufferedReader = null; //velocizza la ricezione e del messaggio
-        BufferedWriter bufferedWriter = null;
-
-        
+        Socket socket;
+        InputStreamReader inputStreamReader;
+        OutputStreamWriter outputStreamWriter;
+        BufferedReader bufferedReader;
+        BufferedWriter bufferedWriter;
         try {
-            Socket = new Socket('localhost',69420); // assegnazione ip + porta
+            socket = new Socket("localhost",2069); // assegnazione ip + porta
 
-            inputStreamReader = new InputStreamReader(Socket.getInputStream());
-            outputStreamWriter = new OutputStreamWriter(Socket.getOutputStream());
+            inputStreamReader = new InputStreamReader(socket.getInputStream());
+            outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
 
             bufferedReader = new BufferedReader(inputStreamReader);
             bufferedWriter = new BufferedWriter(outputStreamWriter);
@@ -33,7 +28,17 @@ public class Client { // stesso nome del file
                 String messaggio_da_inviare = scanner.nextLine();
                 bufferedWriter.write(messaggio_da_inviare);
                 bufferedWriter.newLine();
+                bufferedWriter.flush();
+
+                System.out.println("Server:" + bufferedReader.readLine());
+
+                if (messaggio_da_inviare.equalsIgnoreCase("exit")) //se uscito esci dal while
+                    break;
             }
-        }  
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
